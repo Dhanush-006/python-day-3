36ab823fc04d7c72a3a5dd6634956d29b82781b8
Dhanush-006 <122504741+Dhanush-006@users.noreply.github.com>
2023-01-12 22:40:13 +0530

    Add files via upload

---
+++ dhanush Q3.6 (reveresed string).py
@@ -0,0 +1,11 @@
+def reverse (s):
+    str= ""
+    for i in s:
+        str = i + str
+    return str
+s="1234"
+print("the original string is :",end ="")
+print (s)
+
+print("The reversed string(using loopd) is :",end="")
+print (reverse(s))
